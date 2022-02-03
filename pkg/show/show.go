92518a6005421fbd214ebcbaec5fdb3021f4a599
Hidekazu Karino <hidekazu.karino.j@gmail.com>
2022-02-03 13:01:45 +0000

    rafactor: add Date struct and return Date struct from ValidateInput function

--- pkg/show/show.go
+++ pkg/show/show.go
@@ -5,38 +5,45 @@ import (
 	"mydiary/pkg/util"
 )
 
-func ValidateInput(x int64) error {
+type Date struct {
+	y int64
+	m int64
+	d int64
+}
+
+func ValidateInput(x int64) (date Date, err error) {
 	if x < 10000000 || x >= 100000000 {
-		return fmt.Errorf("input must be format like 20200101")
+		return Date{0, 0, 0}, fmt.Errorf("input must be format like 20200101")
 	}
 	y := x / 10000
 	m := (x % 10000) / 100
 	d := x % 100
 	isLeap := util.IsLeapYear(y)
+	date = Date{y, m, d}
 	if y < 2016 || y > 2200 {
-		return fmt.Errorf("Year must be between 2017 and 2200")
+		return date, fmt.Errorf("Year must be between 2017 and 2200")
 	}
 	if m == 0 || m > 12 {
-		return fmt.Errorf("Month must be between 01 and 12")
+		return date, fmt.Errorf("Month must be between 01 and 12")
 	}
 	if m == 2 {
 		if isLeap {
 			if d == 0 || d > int64(util.DayLengths[m-1])+1 {
-				return fmt.Errorf("Day must be between 01 and 29 (leap year Feburuary)")
+				return date, fmt.Errorf("Day must be between 01 and 29 (leap year Feburuary)")
 			} else {
-				return nil
+				return date, nil
 			}
 		} else {
 			if d == 0 || d > int64(util.DayLengths[m-1]) {
-				return fmt.Errorf("Day must be between 01 and 28 (not leap year Feburuary)")
+				return date, fmt.Errorf("Day must be between 01 and 28 (not leap year Feburuary)")
 			} else {
-				return nil
+				return date, nil
 			}
 		}
 	}
 	if d == 0 || d > int64(util.DayLengths[m-1]) {
-		return fmt.Errorf("Day must be between 01 and %d", util.DayLengths[m-1])
+		return date, fmt.Errorf("Day must be between 01 and %d", util.DayLengths[m-1])
 	}
 
-	return nil
+	return date, nil
 }
--- pkg/show/show_test.go
+++ pkg/show/show_test.go
@@ -4,40 +4,44 @@ import "testing"
 
 func TestValidateInput(t *testing.T) {
 	var result error
-	result = ValidateInput(2000000)
+	_, result = ValidateInput(2000000)
 	if result == nil {
 		t.Errorf("ValidateInput failed at 2000000 %v", result)
 	}
-	result = ValidateInput(200000000)
+	_, result = ValidateInput(200000000)
 	if result == nil {
 		t.Errorf("ValidateInput failed at 200000000 %v", result)
 	}
-	result = ValidateInput(20160000)
+	_, result = ValidateInput(20160000)
 	if result == nil {
 		t.Errorf("ValidateInput failed at 20160000 %v", result)
 	}
-	result = ValidateInput(20170000)
+	_, result = ValidateInput(20170000)
 	if result == nil {
 		t.Errorf("ValidateInput failed at 20170000 %v", result)
 	}
-	result = ValidateInput(20171300)
+	_, result = ValidateInput(20171300)
 	if result == nil {
 		t.Errorf("ValidateInput failed at 20171300 %v", result)
 	}
-	result = ValidateInput(20170100)
+	_, result = ValidateInput(20170100)
 	if result == nil {
 		t.Errorf("ValidateInput failed at 20170100 %v", result)
 	}
-	result = ValidateInput(20170132)
+	_, result = ValidateInput(20170132)
 	if result == nil {
 		t.Errorf("ValidateInput failed at 20170131 %v", result)
 	}
-	result = ValidateInput(20200230)
+	_, result = ValidateInput(20200230)
 	if result == nil {
 		t.Errorf("ValidateInput failed at 20200230 %v", result)
 	}
-	result = ValidateInput(20200229)
+	date, result := ValidateInput(20200229)
+	expected := Date{2020, 2, 29}
 	if result != nil {
 		t.Errorf("ValidateInput failed at 20200229 %v", result)
+	}
+	if date != expected {
+		t.Errorf("ValidateInput expects %v but %v", expected, date)
 	}
 }
