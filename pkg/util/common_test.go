e8d8606112435ebbf8dee6b1a897a631e7d5b8b2
Hidekazu Karino <hidekazu.karino.j@gmail.com>
2023-05-24 23:29:20 +0900

    refactor: change into table driven tests

--- pkg/util/common_test.go
+++ pkg/util/common_test.go
@@ -3,21 +3,41 @@ package util
 import "testing"
 
 func TestIsLeapYear(t *testing.T) {
-	var result bool
-	result = IsLeapYear(2000)
-	if result != true {
-		t.Fatal("failed IsLeapYear year=2000")
+	tests := []struct {
+		name     string
+		year     int64
+		expected bool
+	}{
+		{
+			name:     "2000 is leap year",
+			year:     2000,
+			expected: true,
+		},
+		{
+			name:     "2020 is leap year",
+			year:     2020,
+			expected: true,
+		},
+		{
+			name:     "2021 is not leap year",
+			year:     2021,
+			expected: false,
+		},
+		{
+			name:     "2100 is not leap year",
+			year:     2100,
+			expected: false,
+		},
 	}
-	result = IsLeapYear(2020)
-	if result != true {
-		t.Fatal("failed IsLeapYear year=2020")
-	}
-	result = IsLeapYear(2021)
-	if result != false {
-		t.Fatal("failed IsLeapYear year=2021")
-	}
-	result = IsLeapYear(2100)
-	if result != false {
-		t.Fatal("failed IsLeapYear year=2100")
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			got := IsLeapYear(test.year)
+
+			if got != test.expected {
+				t.Errorf("Unexpected, got: %v, expected: %v", got, test.expected)
+			}
+		})
 	}
 }
